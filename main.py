4231652c5e5de3ad66cc71b724c6a1c1a192bb0e
KANNAPE <bergerthomas38@gmail.com>
2023-05-23 18:37:16 +0200

    Some more exploration of lightbulb.tasks

--- main.py
+++ main.py
@@ -1,16 +1,19 @@
 import hikari
 import lightbulb
+from lightbulb.ext import tasks
 from private import token
 
 bot = lightbulb.BotApp(
     intents=hikari.Intents.ALL,
     token=token.get_token()  # Here, you may want to replace the function by your own token
 )
+tasks.load(bot)
 
 rollus_id = 1109510213570150503
 listus = []
 
 
+# Says something when pinged
 @bot.listen()
 async def ping(event: hikari.GuildMessageCreateEvent) -> None:
     # Do not respond to non-human interaction
@@ -23,13 +26,7 @@ async def ping(event: hikari.GuildMessageCreateEvent) -> None:
         await event.message.respond("Pong!")
 
 
-@bot.command
-@lightbulb.command('ping', 'Says pong, just like mentioning the bot!')
-@lightbulb.implements(lightbulb.SlashCommand)
-async def slash_ping(ctx: lightbulb.Context) -> None:
-    await ctx.respond('Just mention me already...')
-
-
+# Removes a user from the list
 @bot.command
 @lightbulb.option('user', 'User to remove from the list', required=True)
 @lightbulb.command('remove', 'Removes the tagged user from the Basilus Listus.')
@@ -39,6 +36,7 @@ async def remove_from_list(ctx: lightbulb.Context) -> None:
     await ctx.respond('Done')
 
 
+# Display every user that is on the guild
 @bot.command
 @lightbulb.command('list', 'Lists all users of this guild')
 @lightbulb.implements(lightbulb.SlashCommand)
@@ -50,6 +48,7 @@ async def list_all_users(ctx: lightbulb.Context) -> None:
     await ctx.respond(f"Members in this guild: {', '.join(member_names)}")
 
 
+# Display every user that has the special role
 @bot.command
 @lightbulb.command('special-list', 'Lists all users that have the special role.')
 @lightbulb.implements(lightbulb.SlashCommand)
@@ -67,6 +66,14 @@ async def list_special(ctx: lightbulb.Context) -> None:
     await ctx.respond(f"{', '.join(listus)}")
 
 
+# Print a message every 10 seconds
+sunday_trigger = tasks.CronTrigger(day_of_week=0)
+ten_sec_trigger = tasks.CronTrigger(second=10)
+
+
+@tasks.task(trigger=ten_sec_trigger)
+def update_list_on_mondays() -> None:
+    print("le caca")
 
 
 bot.run()
